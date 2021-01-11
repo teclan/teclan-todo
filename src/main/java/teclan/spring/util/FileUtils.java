bb5a7d8b568ec0f521081bf0f31802c5b43b3a2a
谭炳健 <tbj621@163.com>
2021-01-11 09:16:25 +0800

    升级teclan-netty至0.1.0-bate

--- src/main/java/teclan/spring/util/FileUtils.java
+++ src/main/java/teclan/spring/util/FileUtils.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileUtils.class);
@@ -19,5 +21,44 @@ public class FileUtils {
             }
         }
         file.delete();
+    }
+
+    /**
+     * @author Teclan 向文件追加内容，如果文件不存在，创建文件
+     * @param fileName
+     *            文件路径
+     * @param content
+     *            文件内容
+     */
+    public static void write2File(String fileName, String content) {
+        FileWriter writer = null;
+        try {
+            creatIfNeed(fileName);
+            writer = new FileWriter(fileName, true);
+            writer.write(content);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+
+        }
+    }
+
+    public static void creatIfNeed(String fileName) {
+        try {
+            File parentFile = new File(fileName).getParentFile();
+            if (parentFile != null) {
+                parentFile.mkdirs();
+            }
+            new File(fileName).createNewFile();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 }
